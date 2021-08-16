95476d7b5a52552e99f194d6af8a1e4d71dbdcc8
uvi <uvi.artisol@gmail.com>
2021-08-16 15:06:35 +0200

    Added export options arguments

--- src/inquirereport/Main.java
+++ src/inquirereport/Main.java
@@ -12,71 +12,61 @@ import java.util.stream.Collectors;
  * This is the main class of this project. It allows users to create generate
  * CSV or JSON reports out of Inquire data and load them to an SQL server
  *
- * @author Reuven Farchi, Mark Jones, Elizabeth Stovall
+ * @author Reuven Farchi, Elizabeth Stovall, Mark Jones
  */
 public class Main {
     /**
      * @param args command line arguments:
-     *             --config <config.properties> : Mandatory. configuration file or directory e.g. etc/ or test_config.properties. If a directory is selected the application will iterate over all *_config.properties files found in the directory.\n"
-     *             --query <published query>: Optional. name of published query required. This is not case-sensitive. Defaults to the value 'all' which produces all published queries in the LDS.\n"
-     *             --output <output_folder>: Optional. The output location for the reports, e.g. ./output/. System Temp folder will be used as default\n"
-     *             --from <from_date>: Optional. Date for the query search to start from. Valid format is yyyy-MM-ddTHH:mm:ssZ e.g. 2017-08-31T23:55:01Z. Must be have a to_date if used.\n"
-     *             --to <to_date>: Optional. Date for the query search to end. Valid format is yyyy-MM-ddTHH:mm:ssZ e.g. 2017-08-31T23:55:01Z. Must have a from_date if used.\n"
+     *             --help: Optional : Displays command line and property file help.
+     *             --google_sheets: Optional: Loads the data into Google Sheets.
+     *             --azure_sql: Optional: Loads the data into Azure SQL.
+     *             --config  : Optional. configuration file or directory e.g. etc/ or test_config.properties. If a directory is selected the application will iterate over all *_config.properties files found in the directory.Defaults to application root."
+     *             --query : Optional. name of published query required. This is not case-sensitive. Defaults to the value 'all' which produces all published queries in the LDS.\n"
+     *             --from : Optional. Date for the query search to start from. Valid format is yyyy-MM-ddTHH:mm:ssZ e.g. 2017-08-31T23:55:01Z. Must be have a to_date if used.\n"
+     *             --to : Optional. Date for the query search to end. Valid format is yyyy-MM-ddTHH:mm:ssZ e.g. 2017-08-31T23:55:01Z. Must have a from_date if used.\n"
      */
     public static void main(String[] args) throws GeneralSecurityException, IOException {
 
         HashMap<String, String> argsMap = new HashMap<>();
         for (String arg : args) {
-            String[] splitArg = arg.split("--|=");
-            argsMap.put(splitArg[1], splitArg[2]);
+            String[] splitArg = arg.split("-{1,2}|=");
+            argsMap.put(splitArg[1], (splitArg.length > 2 ? splitArg[2]: "" ));
         }
 
-        // Check for the command line arguments
-        if (!argsMap.containsKey("config") || argsMap.containsKey("help")) {
-            System.out.println(
-                    "Parameter usage: \n"
-                            + "Usage: java -jar \"Inquire_Extract.jar\" --config=<config.properties> [--query=<published query> --output=<output_folder> --from=<from_date> --to=<to_date>]\n"
-                            + "\t- config.properties: configuration file or directory e.g. etc/ or test_config.properties. If a directory is selected the application will iterate over all *_config.properties files found in the directory.\n"
-                            + "\t- published query: optional. name of published query required. This is not case-sensitive. Defaults to the value 'all' which produces all published queries in the LDS.\n"
-                            + "\t- output_folder: optional. The output location for the reports, e.g. ./output/. System Temp folder will be used as default\n"
-                            + "\t- from_date: optional. Date for the query search to start from. Valid format is yyyy-MM-ddTHH:mm:ssZ e.g. 2017-08-31T23:55:01Z. Must be have a to_date if used.\n"
-                            + "\t- to_date: optional. Date for the query search to end. Valid format is yyyy-MM-ddTHH:mm:ssZ e.g. 2017-08-31T23:55:01Z. Must have a from_date if used.\n"
-            );
+        if (argsMap.containsKey("help")) {
+            printHelp();
             System.exit(0);
         }
 
-        StringBuilder configFilesPath = new StringBuilder(argsMap.get("config"));
+
+        StringBuilder configFilesPath = new StringBuilder(argsMap.getOrDefault("config", new File(".").getCanonicalPath()));
         boolean isConfigPathDir = new File(configFilesPath.toString()).isDirectory();
 
         String queryName = argsMap.getOrDefault("query", "all");
-
-        String outputFolderPath = argsMap.getOrDefault("output", System.getProperty("java.io.tmpdir") + "/inquire_exporter/");
-        outputFolderPath += (outputFolderPath.charAt(outputFolderPath.length() - 1) == '/' ? "" : "/");
-
         String dateFrom = argsMap.getOrDefault("from", null);
         String dateTo = argsMap.getOrDefault("to", null);
+        boolean exportToSheets = argsMap.containsKey("google_sheets");
+        boolean exportToSql = argsMap.containsKey("azure_sql");
 
 
-        File[] files;
+        File[] configFiles;
         if (isConfigPathDir) {
             configFilesPath.append(configFilesPath.charAt(configFilesPath.length() - 1) == '/' ? "" : "/");
-            files = new File(configFilesPath.toString()).listFiles((dir, name) -> name.endsWith(".properties"));
+            configFiles = new File(configFilesPath.toString()).listFiles((dir, name) -> name.endsWith(".properties"));
         } else {
-            files = new File[1];
-            files[0] = new File(configFilesPath.toString());
+            configFiles = new File[1];
+            configFiles[0] = new File(configFilesPath.toString());
         }
 
-        if (files == null) {
+        if (configFiles == null || configFiles.length == 0) {
+            printHelp();
             throw new IOException("No properties files found in supplied directory.");
         }
-        for (File file : files) {
-            if (file.isFile()) {
-                String fileName = file.getName();
+        for (File configFile : configFiles) {
+            if (configFile.isFile()) {
+                String fileName = configFile.getName();
                 String filePath = configFilesPath + (isConfigPathDir ? fileName : "");
                 System.out.println("Currently processing " + filePath);
-
-                String currentOutputFolderPath = outputFolderPath + (fileName.split("_config")[0] + "/");
-                System.out.println("Temp file path: " + currentOutputFolderPath);
 
                 try {
 
@@ -93,14 +83,26 @@ public class Main {
                     String lds_name = prop.getProperty("lds");
                     String username = prop.getProperty("user");
                     String password = prop.getProperty("password");
+
                     String separator = prop.getProperty("separator");
                     String timeout = prop.getProperty("timeout");
+
+
+                    String outputFolderPath = (String) prop.getOrDefault("outputDir", System.getProperty("java.io.tmpdir") + "/inquire_exporter/");
+                    outputFolderPath += (outputFolderPath.charAt(outputFolderPath.length() - 1) == '/' ? "" : "/") + (fileName.split("_config")[0] + "/");
+
+
+                    System.out.println("Output file path: " + outputFolderPath);
+
+                    String googleCredentialsPath = prop.getProperty("googleCredentialsPath");
+                    String googleCloudAppName = prop.getProperty("googleCloudAppName");
+                    String googleSheetId = prop.getProperty("googleSheetId");
 
 
                     //Check for required
                     boolean mandatoryMissing = false;
                     String missingProperties = "";
-
+                    //General properties
                     if (backend_URL == null) {
                         missingProperties += "\t- backend: mandatory. The URL to the Teneo Inquire backend\n";
                     }
@@ -113,6 +115,28 @@ public class Main {
                     if (password == null) {
                         missingProperties += "\t- password: mandatory. user password to access the LDS\n";
                     }
+
+                    //Specific properties
+                    //Google Sheets
+                    if (googleCredentialsPath == null && exportToSheets) {
+                        missingProperties += "\t- googleCredentialsPath: mandatory. The path to the Google API credentials file.\n";
+                    }
+                    if (googleCloudAppName == null && exportToSheets) {
+                        missingProperties += "\t- googleCloudAppName: mandatory. The name of the Google App to which the service account is linked, e.g. Inquire Exporter.\n";
+                    }
+                    if (googleSheetId == null && exportToSheets) {
+                        missingProperties += "\t- googleSheetId: mandatory. The Id of the Google Sheet document. It is a long hash that can be found in the browser's url bar.\n";
+                    }
+//                    //Azure SQL
+//                    if (azureCredentialsPath == null && exportToSheets) {
+//                        missingProperties += "\t- azureCredentialsPath: mandatory. The path to the Azure API credentials file.\n";
+//                    }
+//                    if (azureCloudAppName == null && exportToSheets) {
+//                        missingProperties += "\t- azureCloudAppName: mandatory. The name of the Azure App to which the service account is linked, e.g. Inquire Exporter.\n";
+//                    }
+//                    if (azureSheetId == null && exportToSheets) {
+//                        missingProperties += "\t- azureSheetId: mandatory. The Id of the Azure Sheet document. It is a long hash that can be found in the browser's url bar.\n";
+//                    }
 
 
                     //Check if any mandatory ones have been missed
@@ -138,11 +162,27 @@ public class Main {
                         }
                     }
 
-                    //Apply defaults
+                    //Apply defaults for export type
                     separator = separator != null ? separator : "json";
                     timeout = timeout != null ? timeout : "30";
                     Thread.sleep(1000);
-                    RunReport.start(queryName, currentOutputFolderPath, dateFrom, dateTo, backend_URL, username, password, lds_name, separator, timeout);
+                    HashMap<String, Iterable<Map<String, Object>>> reportMap = RunReport.start(queryName, dateFrom, dateTo, backend_URL, username, password, lds_name, timeout);
+                    System.out.println(Objects.requireNonNull(reportMap).entrySet());
+
+                    for (Map.Entry<String, Iterable<Map<String, Object>>> report : reportMap.entrySet()) {
+                        if (exportToSheets && !separator.equals("json")) {
+                            OutputFile.fileOutput("json", report, outputFolderPath);
+                        }
+                        if (exportToSql && !separator.equals(",")) {
+                            OutputFile.fileOutput(",", report, outputFolderPath);
+                        }
+                        OutputFile.fileOutput(separator, report, outputFolderPath);
+                    }
+
+                    if (exportToSheets) {
+                        GoogleSheetData.load(googleCredentialsPath, googleCloudAppName, googleSheetId, outputFolderPath);
+                    }
+
                 } catch (Exception e) {
                     System.out.println(e.getMessage());
                     System.out.println(
@@ -152,8 +192,41 @@ public class Main {
                     );
 
                 }
+                printHelp();
 
             }
         }
+    }
+
+    private static void printHelp() {
+        System.out.println(
+                "Parameters:\n"
+                        + "Usage: java -jar \"Inquire_Extract.jar\" [--config=<config.properties> --query=<published query> --from=<from_date> --to=<to_date>]\n"
+                        + "- config.properties: Optional. \n" +
+                        "\tConfiguration file or directory e.g. etc/ or test_config.properties.\n" +
+                        "\tIf a directory is selected the application will iterate over all *_config.properties files found in the directory.\n" +
+                        "\tDefaults to application root.\n"
+                        + "- published query: Optional.\n" +
+                        "\tName of published query to fetch. This is not case-sensitive.\n" +
+                        "\tDefaults to the value 'all' which produces all published queries in the LDS.\n"
+                        + "- from_date: Optional.\n" +
+                        "\tDate for the query search to start from. Valid format is yyyy-MM-ddTHH:mm:ssZ e.g. 2017-08-31T23:55:01Z.\n" +
+                        "\tMust be have a to_date if used.\n"
+                        + "- to_date: Optional.\n" +
+                        "\tDate for the query search to end. Valid format is yyyy-MM-ddTHH:mm:ssZ e.g. 2017-08-31T23:55:01Z.\n" +
+                        "\tMust have a from_date if used.\n\n" +
+                        "Configurations:\n" +
+                        "Usage: create a \n" +
+                        "- backend: mandatory. The URL to the Teneo Inquire backend\n" +
+                        "\t- lds: mandatory. The name of the LDS.\n" +
+                        "\t- username: mandatory. user name to access the LDS\n" +
+                        "\t- password: mandatory. user password to access the LDS\n" +
+                        "\t- googleCredentialsPath: mandatory. The path to the Google API credentials file.\n" +
+                        "\t- googleCloudAppName: mandatory. The name of the Google App to which the service account is linked, e.g. Inquire Exporter.\n" +
+                        "\t- googleSheetId: mandatory. The Id of the Google Sheet document. It is a long hash that can be found in the browser's url bar.\n" +
+                        "\t- separator: optional. Separator used between fields in the output files. Defaults to 'json'.\n" +
+                        "\t- timeout: optional. Timeout for queries. Defaults to 30 seconds."
+
+        );
     }
 }
--- src/inquirereport/RunReport.java
+++ src/inquirereport/RunReport.java
@@ -6,44 +6,23 @@
 package inquirereport;
 
 import com.artisol.teneo.inquire.api.models.SharedQuery;
-import com.artisol.teneo.inquire.client.*;
+import com.artisol.teneo.inquire.client.QueryClient;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.io.IOException;
-import java.util.Collection;
+import java.util.*;
 
 
 public class RunReport {
 
 
-    public static void start(String queryName, String outputFolder, String dateFrom, String dateTo, String backend_URL, String username, String password, String lds_name, String separator, String timeout) {
+    public static HashMap<String, Iterable<Map<String, Object>>> start(String queryName, String dateFrom, String dateTo, String backend_URL, String username, String password, String lds_name, String timeout) {
 
         try {
-            System.out.println("Starting Writing files to " + outputFolder + " at " + new Date());
+            System.out.println("Starting processing data from " + backend_URL + " at " + new Date());
 
             // Login before running reports
             QueryClient clientES = QueryClient.create(backend_URL);
             clientES.login(username, password);
 
-            // Have all information, now run appropriate reports
-            String reportSuffix = "_";
-            if (!(dateFrom == null || dateTo == null)) {
-                reportSuffix += dateFrom + "_" + dateTo;
-            } else {
-                reportSuffix += "all";
-            }
 
             Iterable<Map<String, Object>> results;
 
@@ -54,98 +33,28 @@ public class RunReport {
             usageQueries.add("Usage_–_Sessions");
             usageQueries.add("Usage_–_Standard_Usage");
 
+            HashMap<String, Iterable<Map<String, Object>>> resultsMap = new HashMap<>();
+
             for (SharedQuery publishedQuery : sharedQueries) {
                 String publishedQueryName = publishedQuery.getPublishedName();
                 Thread.sleep(1000);
                 if ((queryName.equalsIgnoreCase(publishedQueryName) || queryName.equalsIgnoreCase("all")) && !usageQueries.contains(publishedQueryName)) {
                     results = PublishedQuery.getOutput(clientES, lds_name, publishedQuery.getQuery(), dateFrom, dateTo, timeout);
                     System.out.println(results);
-                    RunReport.fileOutput(separator, results, publishedQuery.getPublishedName() + reportSuffix, outputFolder);
+                    resultsMap.put(publishedQuery.getPublishedName(), results);
                 }
             }
 
 
             clientES.close();
-            System.out.println("Finished Writing files to " + outputFolder + " at " + new Date());
+            System.out.println("Finished fetching query data at " + new Date());
+            return resultsMap;
 
         } catch (Exception e) {
             System.out.println("ERROR: An error has occurred while processing LDS " + lds_name + ". Will continue processing the others, please run this one again.");
             System.out.println(e.getMessage());
         }
+        return null;
     }
-
-    private static void fileOutput(String fileFormat, Iterable<Map<String, Object>> results, String fileName, String path) {
-
-        //replace bad chars in filename
-        fileName = sanitizeFilename(fileName);
-
-        // create the output folder if it does not exist
-        File directory = new File(String.valueOf(path));
-        if (!directory.exists()) {
-            boolean mkdirsSuccess = directory.mkdirs();
-            System.out.println((mkdirsSuccess ? "Success making " : "Failed to make") + " directory: " + directory.getPath());
-
-        }
-
-        String outputFileName = fileName;
-        if (fileFormat.equalsIgnoreCase("json")) {
-            outputFileName = path.concat(outputFileName).concat(".json");
-
-        } else {
-            outputFileName = path.concat(outputFileName).concat(".txt");
-        }
-
-        System.out.println("Writing to " + outputFileName + " ...");
-
-        if (fileFormat.equalsIgnoreCase("json")) {
-            // Write out to JSON
-            writeResultsToJson(results, outputFileName);
-        } else {
-            // Write out to text files
-            writeResultstoText(results, outputFileName, fileFormat);
-        }
-    }
-
-    private static void writeResultsToJson(Iterable<Map<String, Object>> results, String fullFileName) {
-
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            mapper.writeValue(new File(fullFileName), results);
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    private static void writeResultstoText(Iterable<Map<String, Object>> results,
-                                           String fullFileName,
-                                           String separator) {
-        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(fullFileName), StandardCharsets.UTF_8))) {
-
-            boolean headerDisplayed = false;
-            for (Map<String, Object> map : results) {
-                List<String> header = new ArrayList<>();
-                List<String> row = new ArrayList<>();
-                map.entrySet().stream().forEach((entry) -> {
-                    header.add("\"" + entry.getKey() + "\"");
-                    row.add("\"" + entry.getValue().toString().replace("\"", "\"\"") + "\"");
-                });
-                if (!headerDisplayed) {
-                    writer.write(String.join(separator, header) + System.getProperty("line.separator"));
-                    headerDisplayed = true;
-                }
-                writer.write(String.join(separator, row) + System.getProperty("line.separator"));
-            }
-
-            System.out.println("Writing to " + fullFileName + " complete.");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    private static String sanitizeFilename(String name) {
-        return name.replaceAll("[:\\\\/*?|<>]", "_");
-    }
-
 
 }
