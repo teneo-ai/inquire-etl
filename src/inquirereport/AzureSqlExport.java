b1e3bbce923dfbc4f97933d7b8a462ad20880f17
uvi <uvi.artisol@gmail.com>
2021-08-20 12:54:06 +0200

    Injectable queries to prepared statements.

--- src/inquirereport/AzureSqlExport.java
+++ src/inquirereport/AzureSqlExport.java
@@ -1,10 +1,9 @@
 package inquirereport;
 
+import com.microsoft.sqlserver.jdbc.SQLServerPreparedStatement;
+
 import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,118 +12,122 @@ import java.util.Objects;
 public class AzureSqlExport {
     // Connect to your database.
     // Replace server name, username, and password with your credentials
-    public static void load(String serverName, String dbName, String userName, String password, String dataFolder) {
-        String connectionUrl =
-                "jdbc:sqlserver://" + serverName + ":1433;" +
-                        "database=" + dbName + ";" +
-                        "user=" + userName + ";" +
-                        "password=" + password + ";" +
-                        "encrypt=true;" +
-                        "trustServerCertificate=false;" +
-                        "hostNameInCertificate=*.database.windows.net;" +
-                        "loginTimeout=30;";
+    public static void load(String serverName, String dbName, String userName, String password, String dataFolder) throws SQLException, IOException, InterruptedException {
 
 
-        try {
-            Connection connection = DriverManager.getConnection(connectionUrl);
+//Prepare data
+        java.io.File dir = new java.io.File(dataFolder);
+        java.io.File[] directoryListing = dir.listFiles((_dir, name) -> name.toLowerCase().endsWith(".csv"));
+        if (directoryListing != null) {
+            for (java.io.File child : directoryListing) {
+                Thread.sleep(1000);
+                if (child.isFile()) {
+                    String[] nameElements = child.getName().split("\\.");
+                    String tableName = nameElements[0].substring(0, Math.min(nameElements[0].length(), 127));
+                    List<String[]> csvData = readData(child.getAbsolutePath());
 
-            java.io.File dir = new java.io.File(dataFolder);
-            java.io.File[] directoryListing = dir.listFiles((_dir, name) -> name.toLowerCase().endsWith(".csv"));
-            if (directoryListing != null) {
-                for (java.io.File child : directoryListing) {
-                    Thread.sleep(1000);
-                    if (child.isFile()) {
-                        String[] nameElements = child.getName().split("\\.");
-                        String tableName = nameElements[0].substring(0, Math.min(nameElements[0].length(), 127));
-                        List<String[]> csvData = readData(child.getAbsolutePath());
+                    System.out.println("table name: " + tableName);
 
-                        System.out.println(tableName);
-                        System.out.println(Arrays.toString(csvData.get(0)));
+                    //Get column names and data types
+                    StringBuilder columnTypes = new StringBuilder();
+                    String[] columnNames = csvData.size() > 0 ? csvData.get(0) : new String[0];
+                    System.out.println("column names: " + Arrays.toString(columnNames));
 
 
-                        Statement statement = connection.createStatement();
+                    //Create JDBC connection and statement
+                    String connectionUrl =
+                            "jdbc:sqlserver://" + serverName + ":1433;" +
+                                    "database=" + dbName + ";" +
+                                    "user=" + userName + ";" +
+                                    "password=" + password + ";" +
+                                    "encrypt=true;" +
+                                    "trustServerCertificate=false;" +
+                                    "hostNameInCertificate=*.database.windows.net;" +
+                                    "loginTimeout=30;";
+                    Connection connection = DriverManager.getConnection(connectionUrl);
 
-                        //Get column names and data types
-                        StringBuilder columnTypes = new StringBuilder();
-                        String[] columnNames = csvData.get(0);
-                        for (int i = 0; i < columnNames.length; i++) {
-                            String type = "";
-                            switch (columnNames[i]) {
-                                case "date":
-                                    type = "DATE";
-                                    break;
-                                case "count":
-                                    type = "INT";
-                                    break;
-                                default:
-                                    type = "TEXT";
-                                    break;
-                            }
 
-                            if (i > 0) {
-                                columnTypes.append(", ");
-                            }
-                            columnNames[i] = statement.enquoteIdentifier((columnNames[i].substring(0, Math.min(columnNames[i].length(), 25))), true);
-                            columnTypes.append(columnNames[i]).append(" ").append(type);
+                    for (int i = 0; i < columnNames.length; i++) {
+                        String type;
+                        switch (columnNames[i]) {
+                            case "date":
+                                type = "DATE";
+                                break;
+                            case "count":
+                                type = "INT";
+                                break;
+                            default:
+                                type = "TEXT";
+                                break;
                         }
-                        System.out.println(columnTypes);
 
+                        if (i > 0) {
+                            columnTypes.append(", ");
+                        }
+
+
+                        columnNames[i] = (columnNames[i].substring(0, Math.min(columnNames[i].length(), 25)));
+                        columnTypes.append(columnNames[i]).append(" ").append(type);
+                    }
+                    System.out.println("column types: " + columnTypes);
+
+                    if (csvData.size() > 0) {
+                        //Create table if it doesn't exist
+                        String makeTableQuery =
+                                "IF (NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '" + tableName + "'))\n" +
+                                        "BEGIN CREATE TABLE " + tableName + " (" + columnTypes + ") END";
+
+                        SQLServerPreparedStatement preparedStatement = (SQLServerPreparedStatement) connection.prepareStatement(makeTableQuery);
+                        int result = preparedStatement.executeUpdate();
+                        System.out.println(result > -1 ? "Table " + tableName + " already exists." : "Making table " + tableName +".");
+
+                        String valuesPlaceholderChain = "(" + ("?,".repeat(columnNames.length)).substring(0, (columnNames.length * 2) - 1) + ")";
+
+                        String insertDataQuery = "INSERT INTO " + tableName + "(" + String.join(",", columnNames) + ") VALUES " + valuesPlaceholderChain;
                         //Get Row data
-                        StringBuilder valuesBuilder = new StringBuilder();
+                        preparedStatement = (SQLServerPreparedStatement) connection.prepareStatement(insertDataQuery);
+
                         for (int j = 1; j < csvData.size(); j++) {
                             String[] rowData = csvData.get(j);
-                            if(j != 1){
-                                valuesBuilder.append(", ");
+                            for (int k = 0; k < rowData.length; k++) {
+                                String cellData = rowData[k];
+                                if (Objects.equals(columnNames[k], "\"date\"")) {
+                                    preparedStatement.setDate(k + 1, java.sql.Date.valueOf(cellData));
+                                } else if (Objects.equals(columnNames[k], "\"count\"")) {
+                                    preparedStatement.setInt(k + 1, Integer.parseInt(cellData));
+                                } else {
+                                    preparedStatement.setString(k + 1, cellData);
+                                }
                             }
-                            valuesBuilder.append("(");
-                            for (int h = 0; h < rowData.length; h++) {
-                                String cellData = rowData[h];
-                                if (Objects.equals(columnNames[h], "\"date\"")) {
-                                    cellData = "CONVERT(DATE, '" + cellData + "',23)";
-                                }
-                                else if(!Objects.equals(columnNames[h], "\"count\"")){
-                                    cellData = statement.enquoteLiteral(cellData);
-                                }
-                                if(h != 0){
-                                    valuesBuilder.append(", ");
-                                }
-                                valuesBuilder.append(cellData);
-                            }
-
-                            valuesBuilder.append(")");
+                            preparedStatement.addBatch();
                         }
 
-                        System.out.println(valuesBuilder.toString());
 
-                        //Create table if it doesn't exist
-                        String sqlQuery =
-                                "IF (NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '" + tableName + "'))\n" +
-                                        "BEGIN CREATE TABLE " + tableName + " (" + columnTypes + ") END\n" +
-                                        "INSERT INTO " + tableName + "(" + String.join(",", columnNames) + ") VALUES " + valuesBuilder;
-                        int result = statement.executeUpdate(sqlQuery);
-                        System.out.println("Done. " + result);
+                        int[] rowResults = preparedStatement.executeBatch();
+                        System.out.println("Done. Modified rows: " + Arrays.toString(rowResults));
 
+                        preparedStatement.close();
+                        connection.close();
 
+                    } else {
+                        System.out.println("Table: " + tableName + "was not created or updates because the source file has no data. Please check the report data and the queries on Inquire.");
                     }
-
                 }
+
             }
-
-
-        }
-        // Handle any errors that may have occurred.
-        catch (SQLException | InterruptedException | IOException e) {
-            System.out.println(e.getMessage());
         }
     }
 
     private static List<String[]> readData(String file) throws IOException {
         List<String[]> content = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String line = "";
+            String line;
             while ((line = br.readLine()) != null) {
-                line = line.replace("\"", "");
-                content.add(line.split(","));
+                String[] splitLine = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                for (int c = 0; c < splitLine.length; c++) {
+                    splitLine[c] = splitLine[c].replace("\"", "");
+                }
+                content.add(splitLine);
             }
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
